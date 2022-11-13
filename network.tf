8eedbaed5d8b72a24af6e285e0687727a63513cf
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-13 19:05:49 +0200

    Allow Private Access For Restricted Subnet & Remove Secondary IP Range For Management Subnet

--- network.tf
+++ network.tf
@@ -9,10 +9,7 @@ resource "google_compute_subnetwork" "management-subnet" {
   ip_cidr_range = "10.0.0.0/24"
   region        = "us-central1"
   network       = google_compute_network.project-vpc.id
-  secondary_ip_range {
-    range_name    = "management-subnet-secondary-range"
-    ip_cidr_range = "192.168.0.0/24"
-  }
+  
 }
 
 resource "google_compute_subnetwork" "restricted-subnet" {
@@ -20,6 +17,7 @@ resource "google_compute_subnetwork" "restricted-subnet" {
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.project-vpc.id
+  private_ip_google_access = true
   secondary_ip_range {
     range_name    = "restricted-subnet-secondary-range"
     ip_cidr_range = "192.168.1.0/24"
