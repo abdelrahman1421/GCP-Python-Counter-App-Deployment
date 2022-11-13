bdfa885daca9b8184bb3e044f1ad1b288dce7b69
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-13 18:59:55 +0200

    Create Second Subnet Restricted

--- network.tf
+++ network.tf
@@ -14,3 +14,14 @@ resource "google_compute_subnetwork" "management-subnet" {
     ip_cidr_range = "192.168.0.0/24"
   }
 }
+
+resource "google_compute_subnetwork" "restricted-subnet" {
+  name          = "restricted-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.project-vpc.id
+  secondary_ip_range {
+    range_name    = "restricted-subnet-secondary-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+}
