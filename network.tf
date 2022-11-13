cab6b1869af7094f6eaacb60fa2b13ef528cacdc
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-13 19:07:58 +0200

    Enable Regional Routing For Project VPC

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
 resource "google_compute_network" "project-vpc" {
   name                    = "project-vpc"
   auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
   mtu                     = 1460
 }
 
