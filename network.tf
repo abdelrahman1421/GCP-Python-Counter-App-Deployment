b045c7b864740c4d5fee853f654072080d571234
Abdelrahman Mohamed <abdohamoda636@gmail.com>
2022-11-13 18:59:06 +0200

    Create First Subnet Management

--- network.tf
+++ network.tf
@@ -4,3 +4,13 @@ resource "google_compute_network" "project-vpc" {
   mtu                     = 1460
 }
 
+resource "google_compute_subnetwork" "management-subnet" {
+  name          = "management-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.project-vpc.id
+  secondary_ip_range {
+    range_name    = "management-subnet-secondary-range"
+    ip_cidr_range = "192.168.0.0/24"
+  }
+}
